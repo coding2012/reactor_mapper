3251c970568173b8af7fb60c3b80b802132b245b
Ravi Luthra <coding@gmail.com>
2014-06-09 14:38:53 -0700

    Private constructor

--- src/main/java/org/reactor/mapper/reactor/ex1/MainTest.java
+++ src/main/java/org/reactor/mapper/reactor/ex1/MainTest.java
@@ -4,19 +4,25 @@ import org.reactor.mapper.reactor.OrderMapperContext;
 import reactor.core.Environment;
 import reactor.core.composable.Deferred;
 import reactor.core.composable.Stream;
-import reactor.core.composable.spec.DeferredStreamSpec;
 import reactor.core.composable.spec.Streams;
-import reactor.function.Function;
 
 /**
  * Created by ravi on 6/7/14.
  */
-public class MainTest {
-    public static void main(String[] args) {
-        Environment env = new Environment();
-        Deferred<OrderMapperContext, Stream<OrderMapperContext>> deferred = Streams.<OrderMapperContext>defer().env(env).get();
+public class MainTest
+{
+  private MainTest()
+  {
+     
+  }
 
-        Stream<OrderMapperContext> stream = deferred.compose();
+  public static void main(String[] args)
+  {
+    Environment env = new Environment();
+    Deferred<OrderMapperContext, Stream<OrderMapperContext>> deferred = Streams.<OrderMapperContext>defer().env(env).get();
 
-    }
+    Stream<OrderMapperContext> stream = deferred.compose();
+    //stream.
+
+  }
 }
